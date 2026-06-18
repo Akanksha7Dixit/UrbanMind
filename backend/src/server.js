fd9345b4b5b5a9400dba17078580e7e76b386276
Akanksha7Dixit <akadixit98@gmail.com>
2026-06-19 02:28:40 +0530

    implemented Auth Middleware and Protected Routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const userRoutes =require("./routes/userRoutes");
 
 const app = express();
 
@@ -24,6 +25,11 @@ app.get("/", (req, res) => {
 
 // Auth Routes
 app.use("/api/auth", authRoutes);
+//user routes
+app.use(
+  "/api/users",
+  userRoutes
+);
 
 // Server
 const PORT = process.env.PORT || 5000;
