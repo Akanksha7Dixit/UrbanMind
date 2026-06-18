b7daac9ae3265616a47f209ef6e9b9dd45e6d5e4
Akanksha7Dixit <akadixit98@gmail.com>
2026-06-19 02:03:09 +0530

    implemented password hashing

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 exports.registerUser = async (
@@ -23,12 +24,15 @@ exports.registerUser = async (
       });
     }
 
-    const user =
-      await User.create({
-        name,
-        email,
-        password,
-      });
+    const hashedPassword =
+  await bcrypt.hash(password, 10);
+
+const user =
+  await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
 
     res.status(201).json({
       success: true,
@@ -48,7 +52,48 @@ exports.loginUser = async (
   req,
   res
 ) => {
-  res.json({
-    message: "Login API",
-  });
+  try {
+    const {
+      email,
+      password,
+    } = req.body;
+
+    const user =
+      await User.findOne({
+        email,
+      });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatch =
+      await bcrypt.compare(
+        password,
+        user.password
+      );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    res.json({
+      success: true,
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
